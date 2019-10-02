573b2d22c94712077d0419215639a22dd2280023
pwirth <wirth@hallowelt.biz>
2019-10-02 16:06:49 +0200

    Add hit counter store. ERM:14344

---
+++ src/Data/Page/HitCounter/Record.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace BlueSpice\DistributionConnector\Data\Page\HitCounter;
+
+class Record extends \BlueSpice\Data\Page\Record {
+	const COUNTER = 'page_counter';
+}
---
+++ src/Data/Page/HitCounter/Schema.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace BlueSpice\DistributionConnector\Data\Page\HitCounter;
+
+use BlueSpice\Data\FieldType;
+use BlueSpice\Data\Page\Schema as PageSchema;
+
+class Schema extends \BlueSpice\Data\Schema {
+	const TABLE_NAME = PageSchema::TABLE_NAME;
+	const TABLE_NAME_JOIN = 'hit_counter';
+
+	public function __construct() {
+		parent::__construct( array_merge( (array)( new PageSchema ), [
+			Record::COUNTER => [
+				self::FILTERABLE => true,
+				self::SORTABLE => true,
+				self::TYPE => FieldType::INT
+			],
+		] ) );
+	}
+}
