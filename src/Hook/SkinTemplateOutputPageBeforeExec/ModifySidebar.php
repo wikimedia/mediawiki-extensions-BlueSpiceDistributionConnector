39005f56eb208ea6025e8788e4b23de872c95184
pwirth <wirth@hallowelt.biz>
2019-10-17 12:17:43 +0200

    Add phpcs. ERM:16839
    [3.1.1]
    
    Change-Id: I20cae21a837406b16c18d3b7302d189f737d581b

--- src/Hook/SkinTemplateOutputPageBeforeExec/ModifySidebar.php
+++ src/Hook/SkinTemplateOutputPageBeforeExec/ModifySidebar.php
@@ -7,18 +7,30 @@ use BlueSpice\SkinData;
 
 class ModifySidebar extends SkinTemplateOutputPageBeforeExec {
 
-	protected function doProcess() {
+	/**
+	 *
+	 * @return bool
+	 */
+	protected function skipProcessing() {
+		return !isset( $this->template->data['nav_urls']['duplicator'] );
+	}
 
-		if ( isset( $this->template->data['nav_urls']['duplicator'] ) ) {
-			$this->appendSkinDataArray( SkinData::TOOLBOX_BLACKLIST, 'duplicator' );
-			$this->mergeSkinDataArray( SkinData::EDIT_MENU, [
-					'duplicator' => [
-						//Taken from original Extension:Duplicator codebase
-						'text' => $this->skin->msg( 'duplicator-toolbox' ),
-						'href' => $this->skin->makeSpecialUrl( 'Duplicator', "source=" . wfUrlEncode( "{$this->skin->thispage}" ) )
-				]
-			] );
-		}
+	/**
+	 *
+	 * @return bool
+	 */
+	protected function doProcess() {
+		$this->appendSkinDataArray( SkinData::TOOLBOX_BLACKLIST, 'duplicator' );
+		$this->mergeSkinDataArray( SkinData::EDIT_MENU, [
+				'duplicator' => [
+					// Taken from original Extension:Duplicator codebase
+					'text' => $this->skin->msg( 'duplicator-toolbox' ),
+					'href' => $this->skin->makeSpecialUrl(
+						'Duplicator',
+						"source=" . wfUrlEncode( "{$this->skin->thispage}" )
+					)
+			]
+		] );
 
 		return true;
 	}
