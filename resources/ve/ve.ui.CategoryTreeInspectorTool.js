e50598f49b1ea4ac4fa499cb5d38b500886aa478
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-01-17 09:00:27 +0100

    Add proper dependencies for VE RL module
    [3.1.x]
    
    ERM 17869
    
    Change-Id: Ic61061461623587817f95737dda72ed1764194ae

--- resources/ve/ve.ui.CategoryTreeInspectorTool.js
+++ resources/ve/ve.ui.CategoryTreeInspectorTool.js
@@ -1,22 +1,23 @@
-ve.ui.CategoryTreeInspectorTool = function VeUiCategoryTreeInspectorTool( toolGroup, config ) {
-	ve.ui.CategoryTreeInspectorTool.super.call( this, toolGroup, config );
-};
-OO.inheritClass( ve.ui.CategoryTreeInspectorTool, ve.ui.FragmentInspectorTool );
-ve.ui.CategoryTreeInspectorTool.static.name = 'categoryTreeTool';
-ve.ui.CategoryTreeInspectorTool.static.group = 'none';
-ve.ui.CategoryTreeInspectorTool.static.autoAddToCatchall = false;
-ve.ui.CategoryTreeInspectorTool.static.icon = 'markup';
-ve.ui.CategoryTreeInspectorTool.static.title = OO.ui.deferMsg(
-	'bs-distributionconnector-ve-categorytree-title'
-);
-ve.ui.CategoryTreeInspectorTool.static.modelClasses = [ ve.dm.CategoryTreeNode ];
-ve.ui.CategoryTreeInspectorTool.static.commandName = 'categoryTreeCommand';
-ve.ui.toolFactory.register( ve.ui.CategoryTreeInspectorTool );
+mw.loader.using(["ext.bluespice.visualEditorConnector"], function() {
+	ve.ui.CategoryTreeInspectorTool = function VeUiCategoryTreeInspectorTool(toolGroup, config) {
+		ve.ui.CategoryTreeInspectorTool.super.call(this, toolGroup, config);
+	};
+	OO.inheritClass(ve.ui.CategoryTreeInspectorTool, ve.ui.FragmentInspectorTool);
+	ve.ui.CategoryTreeInspectorTool.static.name = 'categoryTreeTool';
+	ve.ui.CategoryTreeInspectorTool.static.group = 'none';
+	ve.ui.CategoryTreeInspectorTool.static.autoAddToCatchall = false;
+	ve.ui.CategoryTreeInspectorTool.static.icon = 'markup';
+	ve.ui.CategoryTreeInspectorTool.static.title = OO.ui.deferMsg(
+		'bs-distributionconnector-ve-categorytree-title'
+	);
+	ve.ui.CategoryTreeInspectorTool.static.modelClasses = [ve.dm.CategoryTreeNode];
+	ve.ui.CategoryTreeInspectorTool.static.commandName = 'categoryTreeCommand';
+	ve.ui.toolFactory.register(ve.ui.CategoryTreeInspectorTool);
 
-ve.ui.commandRegistry.register(
-	new ve.ui.Command(
-		'categoryTreeCommand', 'window', 'open',
-		{ args: [ 'categoryTreeInspector' ], supportedSelections: [ 'linear' ] }
-	)
-);
-
+	ve.ui.commandRegistry.register(
+		new ve.ui.Command(
+			'categoryTreeCommand', 'window', 'open',
+			{args: ['categoryTreeInspector'], supportedSelections: ['linear']}
+		)
+	);
+} );
