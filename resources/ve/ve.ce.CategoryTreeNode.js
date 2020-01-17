c089db7cd89988921422bc34fd90a37a1c01e108
Dejan Savuljesku <d.savuljesku@gmail.com>
2020-01-17 09:00:27 +0100

    Add proper dependencies for VE RL module
    [3.1.x]
    
    ERM 17869
    
    Change-Id: Ic61061461623587817f95737dda72ed1764194ae

--- resources/ve/ve.ce.CategoryTreeNode.js
+++ resources/ve/ve.ce.CategoryTreeNode.js
@@ -1,42 +1,44 @@
-ve.ce.CategoryTreeNode = function VeCeCategoryTreeNode() {
-	// Parent constructor
-	ve.ce.CategoryTreeNode.super.apply( this, arguments );
-};
+mw.loader.using(["ext.bluespice.visualEditorConnector"], function() {
+	ve.ce.CategoryTreeNode = function VeCeCategoryTreeNode() {
+		// Parent constructor
+		ve.ce.CategoryTreeNode.super.apply( this, arguments );
+	};
 
-/* Inheritance */
+	/* Inheritance */
 
-OO.inheritClass( ve.ce.CategoryTreeNode, ve.ce.MWInlineExtensionNode );
+	OO.inheritClass( ve.ce.CategoryTreeNode, ve.ce.MWInlineExtensionNode );
 
-/* Static properties */
+	/* Static properties */
 
-ve.ce.CategoryTreeNode.static.name = 'categorytree';
+	ve.ce.CategoryTreeNode.static.name = 'categorytree';
 
-ve.ce.CategoryTreeNode.static.primaryCommandName = 'categorytree';
+	ve.ce.CategoryTreeNode.static.primaryCommandName = 'categorytree';
 
 // If body is empty, tag does not render anything
-ve.ce.CategoryTreeNode.static.rendersEmpty = false;
+	ve.ce.CategoryTreeNode.static.rendersEmpty = false;
 
-/**
- * @inheritdoc
- */
-ve.ce.CategoryTreeNode.prototype.onSetup = function () {
-	// Parent method
-	ve.ce.CategoryTreeNode.super.prototype.onSetup.call( this );
+	/**
+	 * @inheritdoc
+	 */
+	ve.ce.CategoryTreeNode.prototype.onSetup = function () {
+		// Parent method
+		ve.ce.CategoryTreeNode.super.prototype.onSetup.call( this );
 
-	// DOM changes
-	this.$element.addClass( 've-ce-categorytreenode' );
-};
+		// DOM changes
+		this.$element.addClass( 've-ce-categorytreenode' );
+	};
 
-/**
- * @inheritdoc ve.ce.GeneratedContentNode
- */
-ve.ce.CategoryTreeNode.prototype.validateGeneratedContents = function ( $element ) {
-	if ( $element.is( 'div' ) && $element.hasClass( 'errorbox' ) ) {
-		return false;
-	}
-	return true;
-};
+	/**
+	 * @inheritdoc ve.ce.GeneratedContentNode
+	 */
+	ve.ce.CategoryTreeNode.prototype.validateGeneratedContents = function ( $element ) {
+		if ( $element.is( 'div' ) && $element.hasClass( 'errorbox' ) ) {
+			return false;
+		}
+		return true;
+	};
 
-/* Registration */
+	/* Registration */
 
-ve.ce.nodeFactory.register( ve.ce.CategoryTreeNode );
+	ve.ce.nodeFactory.register( ve.ce.CategoryTreeNode );
+} ) ;
--- resources/ve/ve.dm.CategoryTreeNode.js
+++ resources/ve/ve.dm.CategoryTreeNode.js
@@ -1,25 +1,27 @@
-ve.dm.CategoryTreeNode = function VeDmCategoryTreeNode() {
-	// Parent constructor
-	ve.dm.CategoryTreeNode.super.apply( this, arguments );
-};
+mw.loader.using(["ext.bluespice.visualEditorConnector"], function() {
+	ve.dm.CategoryTreeNode = function VeDmCategoryTreeNode() {
+		// Parent constructor
+		ve.dm.CategoryTreeNode.super.apply(this, arguments);
+	};
 
-/* Inheritance */
+	/* Inheritance */
 
-OO.inheritClass( ve.dm.CategoryTreeNode, ve.dm.MWInlineExtensionNode );
+	OO.inheritClass(ve.dm.CategoryTreeNode, ve.dm.MWInlineExtensionNode);
 
-/* Static members */
+	/* Static members */
 
-ve.dm.CategoryTreeNode.static.name = 'categorytree';
+	ve.dm.CategoryTreeNode.static.name = 'categorytree';
 
-ve.dm.CategoryTreeNode.static.tagName = 'categorytree';
+	ve.dm.CategoryTreeNode.static.tagName = 'categorytree';
 
 // Name of the parser tag
-ve.dm.CategoryTreeNode.static.extensionName = 'categorytree';
+	ve.dm.CategoryTreeNode.static.extensionName = 'categorytree';
 
 // This tag renders without content
-ve.dm.CategoryTreeNode.static.childNodeTypes = [];
-ve.dm.CategoryTreeNode.static.isContent = true;
+	ve.dm.CategoryTreeNode.static.childNodeTypes = [];
+	ve.dm.CategoryTreeNode.static.isContent = true;
 
-/* Registration */
+	/* Registration */
 
-ve.dm.modelRegistry.register( ve.dm.CategoryTreeNode );
+	ve.dm.modelRegistry.register(ve.dm.CategoryTreeNode);
+} );
