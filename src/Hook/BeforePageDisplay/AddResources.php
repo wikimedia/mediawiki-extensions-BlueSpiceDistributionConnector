1f0134e4bb0a23aa72891d630a221ecb27ddb06f
rvogel <vogel@hallowelt.biz>
2021-02-17 10:19:57 +0100

    Remove legacy `Extension:LdapAuthentication` integration
    ERM23063
    
    [3.2.0]
    
    Change-Id: I7f6c1fae81a4306aa5bfa6829ac2c6f284bc98f7

--- src/Hook/BeforePageDisplay/AddResources.php
+++ src/Hook/BeforePageDisplay/AddResources.php
@@ -12,14 +12,6 @@ class AddResources extends BeforePageDisplay {
 	 */
 	protected function doProcess() {
 		$this->out->addModules( 'ext.bluespice.distribution.styles' );
-		$spFactory = $this->getServices()->getSpecialPageFactory();
-		if ( !$spFactory->exists( 'Userlogin' ) ) {
-			return true;
-		}
-		$userLogin = $spFactory->getPage( 'Userlogin' );
-		if ( $this->out->getTitle()->equals( $userLogin->getPageTitle() ) ) {
-			$this->out->addModules( 'ext.bluespice.distribution.ldap' );
-		}
 		return true;
 	}
 
