4f12fa971d8feba829393009d07819f3ca0204ad
pwirth <wirth@hallowelt.biz>
2021-09-14 15:03:57 +0200

    Add UserMerge hook base classes
    Requires:
    https://github.com/hallowelt/mediawiki/pull/192
    https://github.com/hallowelt/mediawiki/pull/193
    https://github.com/hallowelt/mediawiki/pull/194
    
    ERM:16839
    [3.2.x]
    
    Change-Id: Icc2c1449c7ab2c4dabc6976dac384136c0c069cc
    (cherry picked from commit 6ac689d0b2fb5c43bb873b0db9bf8dd8c7c377f1)

---
+++ src/Hook/DeleteAccount.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace BlueSpice\DistributionConnector\Hook;
+
+use BlueSpice\Hook;
+use Config;
+use IContextSource;
+use User;
+
+abstract class DeleteAccount extends Hook {
+	/**
+	 *
+	 * @var User
+	 */
+	protected $oldUser = null;
+
+	/**
+	 *
+	 * @param User &$oldUser
+	 * @return bool
+	 */
+	public static function callback( &$oldUser ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			null,
+			null,
+			$oldUser
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 *
+	 * @param IContextSource $context
+	 * @param Config $config
+	 * @param User &$oldUser
+	 */
+	public function __construct( $context, $config, &$oldUser ) {
+		parent::__construct( $context, $config );
+
+		$this->oldUser = &$oldUser;
+	}
+}
