9d63602b885b805bad8bc8a970d4789bbdd45136
pwirth <wirth@hallowelt.biz>
2021-09-14 15:03:57 +0200

    Add UserMerge hook base classes
    Requires:
    https://github.com/hallowelt/mediawiki/pull/192
    https://github.com/hallowelt/mediawiki/pull/193
    https://github.com/hallowelt/mediawiki/pull/194
    
    ERM:16839
    [3.2.x]
    
    Change-Id: Icc2c1449c7ab2c4dabc6976dac384136c0c069cc
    (cherry picked from commit 6ac689d0b2fb5c43bb873b0db9bf8dd8c7c377f1)
    (cherry picked from commit 4f12fa971d8feba829393009d07819f3ca0204ad)

---
+++ src/Hook/UserMergeAccountFields.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace BlueSpice\DistributionConnector\Hook;
+
+use BlueSpice\Hook;
+use Config;
+use IContextSource;
+
+abstract class UserMergeAccountFields extends Hook {
+	/**
+	 *
+	 * @var array
+	 */
+	protected $updateFields = null;
+
+	/**
+	 *
+	 * @param array &$updateFields
+	 * @return bool
+	 */
+	public static function callback( &$updateFields ) {
+		$className = static::class;
+		$hookHandler = new $className(
+			null,
+			null,
+			$updateFields
+		);
+		return $hookHandler->process();
+	}
+
+	/**
+	 *
+	 * @param IContextSource $context
+	 * @param Config $config
+	 * @param array &$updateFields
+	 */
+	public function __construct( $context, $config, &$updateFields ) {
+		parent::__construct( $context, $config );
+
+		$this->updateFields = &$updateFields;
+	}
+}
