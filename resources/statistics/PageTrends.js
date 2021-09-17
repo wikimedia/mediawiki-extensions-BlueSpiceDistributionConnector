d0891d62687ecf8dd285b3b5a830bc405b0a917c
d.savuljesku <savuljesku@hallowelt.com>
2021-09-17 08:45:04 +0200

    Implement new Statistics snapshot providers
    Requires I999788b1dfef1ac20cf6df6a265b2983ab571e50
    
    [3.2.x]
    
    ERM 25753
    
    Change-Id: I63556c031fb4710676f850a6954f8527706d1653

---
+++ resources/statistics/PageTrends.js
@@ -0,0 +1,18 @@
+(function ( mw, $, bs) {
+	bs.util.registerNamespace( 'bs.distributionConnector.report' );
+
+	bs.distributionConnector.report.PageTrendsReport = function ( cfg ) {
+		bs.distributionConnector.report.PageTrendsReport.parent.call( this, cfg );
+	};
+
+	OO.inheritClass( bs.distributionConnector.report.PageTrendsReport, bs.distributionConnector.report.PageHitsReport );
+
+	bs.distributionConnector.report.PageTrendsReport.static.label = mw.message( "bs-distributionconnector-statistics-report-page-trends" ).text();
+
+	bs.distributionConnector.report.PageTrendsReport.prototype.getAxisLabels = function () {
+		return {
+			value: mw.message( "bs-distributionconnector-statistics-report-page-trends-axis-value" ).text()
+		};
+	};
+
+} )( mediaWiki, jQuery , blueSpice);
