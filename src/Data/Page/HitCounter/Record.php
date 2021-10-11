3c580fc533f622aae80c3ead5d6a76a7d7a422b2
Alexander Vorwerk <alexander.vorwerk@stud.uni-goettingen.de>
2021-10-11 13:55:33 +0200

    Add visibility to constants
    We support PHP 7.3.19 or later, so we should declare the visibility of
    constants.
    
    Bug: T237840
    Change-Id: I964acb42c9411d4879914a41e0ab0903c8fe0b5f

--- src/Data/Page/HitCounter/Record.php
+++ src/Data/Page/HitCounter/Record.php
@@ -3,5 +3,5 @@
 namespace BlueSpice\DistributionConnector\Data\Page\HitCounter;
 
 class Record extends \BlueSpice\Data\Page\Record {
-	const COUNTER = 'page_counter';
+	public const COUNTER = 'page_counter';
 }
