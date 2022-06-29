16d1eea89532baedb51ac63aaea5c9c63a1f7273
miriam schlindwein <schlindwein@hallowelt.com>
2022-06-29 14:03:00 +0200

    Add reference to specialpage InviteSignup
    ERM28260
    
    [4.x]
    
    Change-Id: I03a43f2bf71461804850b2ec30da32d42edb924d
    (cherry picked from commit 7042bca09a3ff59eda60d5f21cd439fd951b3e61)
    (cherry picked from commit 7ffa42ca96d69845ab5cfbe44a0fdd6c9ae4cadf)

---
+++ src/Hook/AddInviteLink.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace BlueSpice\DistributionConnector\Hook;
+
+use BlueSpice\UserManager\Hook\BSUserManagerRegisterModules;
+use ExtensionRegistry;
+
+class AddInviteLink implements BSUserManagerRegisterModules {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onBSUserManagerRegisterModules( &$modules, $user ): void {
+		if ( !$this->isHidden() ) {
+			$modules[] = "ext.bluespice.distribution.usermanager.invite";
+		}
+	}
+
+	/**
+	 *
+	 * @return bool
+	 */
+	private function isHidden() {
+		return !ExtensionRegistry::getInstance()->isLoaded( 'InviteSignup' );
+	}
+
+}
