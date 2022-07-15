78893a633b2c3446e2b88ec1aef4d689294b147a
rvogel <vogel@hallowelt.biz>
2022-07-15 10:36:11 +0200

    Fix language fallback logic
    We now allow injecting the available laguages. This may be different for
    each content manifest file.
    
    Also "de" is not a fallback for "de", but only for "de-formal".
    Therefore the old logic fell back to "en" for "de".
    
    ERM28276
    
    [4.x]
    
    Change-Id: I569151d6dff5ec6a198e2cdccb0d8e1faaf718be
    (cherry picked from commit 8871a9fc152697f258c5a4cd25485eabd0983807)

--- src/ContentImport/ImportLanguage.php
+++ src/ContentImport/ImportLanguage.php
@@ -7,17 +7,6 @@ use MediaWiki\Languages\LanguageFallback;
 class ImportLanguage {
 
 	/**
-	 * Languages which can be used to import default BlueSpice pages.
-	 * Currently "en" and "de" languages are the only languages, on which this content is available.
-	 *
-	 * @var string[]
-	 */
-	private $importLanguages = [
-		'en',
-		'de'
-	];
-
-	/**
 	 * Language fallback service.
 	 *
 	 * @var LanguageFallback
@@ -43,13 +32,17 @@ class ImportLanguage {
 	/**
 	 * Returns fallback language (depending on language of wiki)
 	 *
+	 * @param array $availableLanguages
 	 * @return string Fallback language, "en" if no fallbacks available (in case of unknown language, for example)
 	 * @see LanguageFallback::getAll()
 	 */
-	public function getImportLanguage(): string {
+	public function getImportLanguage( $availableLanguages ): string {
+		if ( in_array( $this->wikiLangCode, $availableLanguages ) ) {
+			return $this->wikiLangCode;
+		}
 		$fallbackLanguages = $this->languageFallback->getAll( $this->wikiLangCode );
 		foreach ( $fallbackLanguages as $fallbackLanguage ) {
-			if ( in_array( $fallbackLanguage, $this->importLanguages ) ) {
+			if ( in_array( $fallbackLanguage, $availableLanguages ) ) {
 				return $fallbackLanguage;
 			}
 		}
