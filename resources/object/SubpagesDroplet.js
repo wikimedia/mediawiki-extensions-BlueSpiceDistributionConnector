26ae3b797b270dd2f609d89ddd52cdd10db90be6
miriam schlindwein <schlindwein@hallowelt.com>
2022-08-23 09:20:54 +0200

    Add subpages droplet
    ERM29376
    
    [4.x]
    
    Change-Id: Ia017d90ed7e5b3c6bf7efa826b2c847d533615fd

---
+++ resources/object/SubpagesDroplet.js
@@ -0,0 +1,58 @@
+( function ( mw, $, bs ) {
+	bs.util.registerNamespace( 'bs.distributionConnector.object' );
+
+	bs.distributionConnector.object.SubpagesDroplet = function( cfg ) {
+		bs.distributionConnector.object.SubpagesDroplet.parent.call( this, cfg );
+	};
+
+	OO.inheritClass( bs.distributionConnector.object.SubpagesDroplet, ext.contentdroplets.object.TransclusionDroplet );
+
+	bs.distributionConnector.object.SubpagesDroplet.prototype.templateMatches = function( templateData ) {
+		if ( !templateData ) {
+			return false;
+		}
+		var target = templateData.target.wt;
+
+		return target.trim( '\n' ) === 'Subpages' && 'subpages' === this.getKey();
+	};
+
+
+	bs.distributionConnector.object.SubpagesDroplet.prototype.toDataElement = function( domElements, converter  ) {
+		return false;
+	};
+
+	bs.distributionConnector.object.SubpagesDroplet.prototype.getFormItems = function() {
+
+		var formItems = [
+			{
+				name: 'parentnamespace',
+				label: mw.message( 'droplets-subpages-namespace-label' ).plain(),
+				type: 'text',
+				help: mw.message( 'droplets-subpages-namespace-help' ).plain()
+			},
+			{
+				name: 'parentpage',
+				label: mw.message( 'droplets-subpages-parentpage-label' ).plain(),
+				type: 'text',
+				help: mw.message( 'droplets-subpages-parentpage-help' ).plain()
+			},
+			{
+				name: 'cols',
+				label: mw.message( 'droplets-subpages-cols-label' ).plain(),
+				type: 'text',
+				help: mw.message( 'droplets-subpages-cols-help' ).plain()
+			},
+			{
+				name: 'bullets',
+				label: mw.message( 'droplets-subpages-bullets-label' ).plain(),
+				type: 'text',
+				help: mw.message( 'droplets-subpages-bullets-help' ).plain()
+			}
+		];
+
+		return formItems;
+	};
+
+	ext.contentdroplets.registry.register( 'subpages', bs.distributionConnector.object.SubpagesDroplet );
+
+} )( mediaWiki, jQuery, blueSpice );
