983fd12bb93e27ca4b0bdbed1415cf670b9b8f4a
miriam schlindwein <schlindwein@hallowelt.com>
2022-10-12 10:26:11 +0200

    Fix page hits
    Add page hits in relation
    
    ERM29537
    
    [4.2.x]
    
    Change-Id: I68da1da0e0421905d074b79fd2651106693b4d94
    (cherry picked from commit 623b54638bb2bda43e0cca4519a4c7c60667e34a)

---
+++ resources/statistics/PageHitsRelation.js
@@ -0,0 +1,30 @@
+(function ( mw, $, bs) {
+	bs.util.registerNamespace( 'bs.distributionConnector.report' );
+
+	bs.distributionConnector.report.PageHitsRelationReport = function ( cfg ) {
+		bs.distributionConnector.report.PageHitsRelationReport.parent.call( this, cfg );
+	};
+
+	OO.inheritClass( bs.distributionConnector.report.PageHitsRelationReport, bs.aggregatedStatistics.report.ReportBase );
+
+	bs.distributionConnector.report.PageHitsRelationReport.static.label =
+		mw.message( "bs-distributionconnector-statistics-report-page-hits-relation" ).text();
+
+	bs.distributionConnector.report.PageHitsRelationReport.prototype.getFilters = function () {
+		return [
+			new bs.aggregatedStatistics.filter.IntervalFilter(),
+			new bs.aggregatedStatistics.filter.PageFilter( { required: true } )
+		];
+	};
+
+	bs.distributionConnector.report.PageHitsRelationReport.prototype.getChart = function () {
+		return new bs.aggregatedStatistics.charts.LineChart();
+	};
+
+	bs.distributionConnector.report.PageHitsRelationReport.prototype.getAxisLabels = function () {
+		return {
+			value: mw.message( "bs-distributionconnector-statistics-report-page-hits-relation-axis-label" ).text()
+		};
+	};
+
+} )( mediaWiki, jQuery , blueSpice);
