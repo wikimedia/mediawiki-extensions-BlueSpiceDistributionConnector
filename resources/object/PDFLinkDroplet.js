d9811c3ff4be32a9023ffb03bf7358f6fa7d7dc5
miriam schlindwein <schlindwein@hallowelt.com>
2022-12-09 10:13:25 +0100

    Add PDF link droplet
    ERM29382
    
    [4.x]
    
    Change-Id: I42bc0f5820ba8f8b3c8a2090de990f8d2583b232

---
+++ resources/object/PDFLinkDroplet.js
@@ -0,0 +1,58 @@
+( function ( mw, $, bs ) {
+
+	bs.util.registerNamespace( 'bs.distributionConnector.object' );
+
+	bs.distributionConnector.object.PDFLinkDroplet = function( cfg ) {
+		bs.distributionConnector.object.PDFLinkDroplet.parent.call( this, cfg );
+	};
+
+	OO.inheritClass( bs.distributionConnector.object.PDFLinkDroplet, ext.contentdroplets.object.TransclusionDroplet );
+
+	bs.distributionConnector.object.PDFLinkDroplet.prototype.templateMatches = function( templateData ) {
+		if ( !templateData ) {
+			return false;
+		}
+		var target = templateData.target.wt;
+		return target.trim( '\n' ) === 'PDFLink';
+	};
+
+	bs.distributionConnector.object.PDFLinkDroplet.prototype.toDataElement = function( domElements, converter  ) {
+		return false;
+	};
+
+	bs.distributionConnector.object.PDFLinkDroplet.prototype.getFormItems = function() {
+		var defaultTemplate = mw.config.get( 'bsUEModulePDFDefaultTemplate' );
+		var availableTemplate = mw.config.get( 'bsUEModulePDFAvailableTemplates');
+
+		var templates = [];
+		for ( var entry in availableTemplate ) {
+			var item =  {
+				data: availableTemplate[ entry ]
+			};
+			templates.push( item );
+		}
+
+		return [
+			{
+				name: 'page',
+				label: mw.message( 'droplets-pdf-link-page-label' ).plain(),
+				type: 'text'
+			},
+			{
+				name: 'template',
+				label: mw.message( 'droplets-pdf-link-template-label' ).plain(),
+				type: 'dropdown',
+				default: defaultTemplate,
+				options: templates
+			},
+			{
+				name: 'label',
+				label: mw.message( 'droplets-pdf-link-label-label' ).plain(),
+				type: 'text'
+			}
+		];
+	};
+
+	ext.contentdroplets.registry.register( 'pdflink', bs.distributionConnector.object.PDFLinkDroplet );
+
+} )( mediaWiki, jQuery, blueSpice );
