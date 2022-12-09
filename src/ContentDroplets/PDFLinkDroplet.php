4a76f0b9fa1422e6735404eaff6be9fbae57e758
miriam schlindwein <schlindwein@hallowelt.com>
2022-12-09 10:13:25 +0100

    Add PDF link droplet
    ERM29382
    
    [4.x]
    
    Change-Id: I42bc0f5820ba8f8b3c8a2090de990f8d2583b232

---
+++ src/ContentDroplets/PDFLinkDroplet.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace BlueSpice\DistributionConnector\ContentDroplets;
+
+use MediaWiki\Extension\ContentDroplets\Droplet\TemplateDroplet;
+use Message;
+
+class PDFLinkDroplet extends TemplateDroplet {
+
+	/**
+	 * Get target for the template
+	 * @return string
+	 */
+	protected function getTarget(): string {
+		return 'PDFLink';
+	}
+
+	/**
+	 * Template params
+	 * @return array
+	 */
+	protected function getParams(): array {
+		return [
+			'page' => 'Main Page',
+			'template' => 'BlueSpice',
+			'label' => 'PDF Link'
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getName(): Message {
+		return Message::newFromKey( 'droplets-pdflink-name' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getDescription(): Message {
+		return Message::newFromKey( 'droplets-pdflink-description' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getIcon(): string {
+		return 'droplet-pdf-link';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getRLModules(): array {
+		return [ 'ext.bluespice.distribution.droplet.pdflink' ];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getCategories(): array {
+		return [ 'content', 'export' ];
+	}
+}
