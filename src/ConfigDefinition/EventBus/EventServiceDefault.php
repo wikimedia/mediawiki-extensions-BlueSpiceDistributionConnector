53ab1507faceaad1f7eb37ba805b030a24f8f70a
Markus Glaser <glaser@hallowelt.com>
2023-02-14 11:13:22 +0100

    Disable EventBus by default and use reasonable settings
    Eventbus causes some severe performance issues on Windows if not
    configured correctly. So we disable it by default, add resonable default
    settings and expose all configs which are required to configure it.
    
    ERM 31349
    [4.2.x]
    
    Change-Id: I029a445124022c4517ebe9fd356307d0f5c1e779

---
+++ src/ConfigDefinition/EventBus/EventServiceDefault.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace BlueSpice\DistributionConnector\ConfigDefinition\EventBus;
+
+use BlueSpice\ConfigDefinition\IOverwriteGlobal;
+use BlueSpice\ConfigDefinition\StringSetting;
+use BlueSpice\DistributionConnector\ISettingPaths;
+use ExtensionRegistry;
+
+class EventServiceDefault extends StringSetting implements ISettingPaths, IOverwriteGlobal {
+	private const EXTENSION_EVENT_BUS = 'EventBus';
+
+	/**
+	 * @return string[]
+	 */
+	public function getPaths() {
+		return [
+			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_SYSTEM . '/' . self::EXTENSION_EVENT_BUS,
+			static::MAIN_PATH_EXTENSION . '/' . self::EXTENSION_EVENT_BUS . '/' . static::FEATURE_SYSTEM,
+			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_CLOUD . '/' . self::EXTENSION_EVENT_BUS,
+		];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLabelMessageKey() {
+		return 'bs-distributionconnector-pref-eventbus-eventservicedefault';
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getHelpMessageKey() {
+		return 'bs-distributionconnector-pref-eventbus-eventservicedefault-help';
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isHidden() {
+		return !ExtensionRegistry::getInstance()->isLoaded( self::EXTENSION_EVENT_BUS );
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function getGlobalName() {
+		return "wgEventServiceDefault";
+	}
+}
