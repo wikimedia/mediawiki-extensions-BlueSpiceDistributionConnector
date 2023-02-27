9cd6afee2204c4cb5811f6c5a982e7b7d6cf60c3
miriam schlindwein <schlindwein@hallowelt.com>
2023-02-27 11:47:58 +0100

    Add create input droplet
    ERM31357
    
    [4.x]
    
    Change-Id: I4b07851e0dcb9d6f1c5d544470623e88ed7ef7df
    (cherry picked from commit 41935e544c2575a23c0a49e67e4a0dc3bbeca692)

---
+++ src/ContentDroplets/CreateInputDroplet.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace BlueSpice\DistributionConnector\ContentDroplets;
+
+use MediaWiki\Extension\ContentDroplets\Droplet\TemplateDroplet;
+use Message;
+
+class CreateInputDroplet extends TemplateDroplet {
+
+	/**
+	 */
+	public function __construct() {
+	}
+
+	/**
+	 * Get target for the template
+	 * @return string
+	 */
+	protected function getTarget(): string {
+		return 'CreateInput';
+	}
+
+	/**
+	 * Template params
+	 * @return array
+	 */
+	protected function getParams(): array {
+		return [
+			'alignment' => '',
+			'buttonlabel' => 'Create',
+			'preload' => '',
+			'placeholder' => 'Enter page name',
+			'prefix' => 'prefix'
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getName(): Message {
+		return Message::newFromKey( 'droplets-create-input-name' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getDescription(): Message {
+		return Message::newFromKey( 'droplets-create-input-desc' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getIcon(): string {
+		return 'droplet-create-input';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getRLModules(): array {
+		return [ 'ext.bluespice.distribution.droplet.createInput' ];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getCategories(): array {
+		return [ 'content', 'navigation' ];
+	}
+}
