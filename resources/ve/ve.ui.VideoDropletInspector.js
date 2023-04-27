4c660d58cf77292c032b1e958b6e4071d86ad112
miriam schlindwein <schlindwein@hallowelt.com>
2023-04-27 12:20:35 +0200

    Add missing params to video inspector
    ERM32406
    
    [4.3.x]
    
    Change-Id: Ia4e2f16d88de51f4a0259e5823fb48b128e07bde
    (cherry picked from commit 312f1e540bf467326528580f06885852cf97ce18)

--- resources/ve/ve.ui.VideoDropletInspector.js
+++ resources/ve/ve.ui.VideoDropletInspector.js
@@ -22,11 +22,8 @@ ve.ui.VideoDropletInspector.static.selfCloseEmptyBody = false;
 
 ve.ui.VideoDropletInspector.prototype.initialize = function () {
 	ve.ui.VideoDropletInspector.super.prototype.initialize.call( this );
-	// Add label for input field
-	let label = new OO.ui.LabelWidget( {
-		label: mw.message( 'bs-distributionconnector-videodropletinspector-video-link-label' ).text()
-	});
-	this.form.$element.prepend( label.$element );
+	// remove input field
+	this.input.$element.remove();
 
 	this.indexLayout = new OO.ui.PanelLayout( {
 		scrollable: false,
@@ -42,7 +39,12 @@ ve.ui.VideoDropletInspector.prototype.initialize = function () {
 	this.$content.addClass( 've-ui-videoDropletInspector-content' );
 
 	this.indexLayout.$element.append(
+		this.inputLayout.$element,
 		this.serviceLayout.$element,
+		this.descriptionLayout.$element,
+		this.dimensionLayout.$element,
+		this.alignmentLayout.$element,
+		this.containerLayout.$element,
 		this.generatedContentsError.$element
 	);
 	this.form.$element.append(
@@ -51,11 +53,15 @@ ve.ui.VideoDropletInspector.prototype.initialize = function () {
 };
 
 ve.ui.VideoDropletInspector.prototype.createFields = function () {
-	var services = mw.config.get( 'bsgEmbedVideoServices' );
+	this.input = new ve.ui.WhitespacePreservingTextInputWidget( {
+		rows: 2
+	} );
 
-	var serviceData = [];
-	for ( var service in services ) {
-		var item =  new OO.ui.MenuOptionWidget( {
+	let services = mw.config.get( 'bsgEmbedVideoServices' );
+
+	let serviceData = [];
+	for ( let service in services ) {
+		let item =  new OO.ui.MenuOptionWidget( {
 			data: services[ service ],
 			label: services[ service ]
 		} );
@@ -67,12 +73,70 @@ ve.ui.VideoDropletInspector.prototype.createFields = function () {
 		}
 	} );
 	this.serviceInput.getMenu().selectItemByData( 'youtube' );
+
+	this.descriptionInput = new OO.ui.MultilineTextInputWidget( {
+		rows: 2,
+		placeholder: mw.message( 'bs-distributionconnector-videodropletinspector-description-placeholder' ).plain()
+	} );
+	this.dimensionsInput = new OO.ui.TextInputWidget( {
+		placeholder: '640'
+	} );
+
+	let alignmentsData = [ 'left', 'center', 'right' ];
+	let alignments = [];
+	for ( let alignmentKey in alignmentsData ) {
+		let alignment = alignmentsData[ alignmentKey ];
+		let item =  new OO.ui.MenuOptionWidget( {
+			data: alignment,
+			// bs-distributionconnector-videodropletinspector-alignment-left
+			// bs-distributionconnector-videodropletinspector-alignment-center
+			// bs-distributionconnector-videodropletinspector-alignment-right
+			label: mw.message(
+				"bs-distributionconnector-videodropletinspector-alignment-" + alignment ).plain()
+		} );
+		alignments.push( item );
+	}
+	this.alignmentInput = new OO.ui.DropdownWidget( {
+		menu: {
+			items: alignments
+		}
+	} );
+	this.alignmentInput.getMenu().selectItemByData( 'left' );
+	this.containerInput = new OO.ui.CheckboxInputWidget( {
+		value: 'frame'
+	} );
 }
 
 ve.ui.VideoDropletInspector.prototype.setLayouts = function () {
+	this.inputLayout = new OO.ui.FieldLayout( this.input, {
+		align: 'top',
+		label: mw.message( 'bs-distributionconnector-videodropletinspector-video-link-label' ).plain()
+	});
 	this.serviceLayout = new OO.ui.FieldLayout( this.serviceInput, {
-		align: 'right',
-		label: mw.message( 'bs-distributionconnector-videodropletinspector-service-label' ).plain()
+		align: 'left',
+		label: mw.message( 'bs-distributionconnector-videodropletinspector-service-label' ).plain(),
+		help: mw.message( 'bs-distributionconnector-videodropletinspector-service-help' ).plain()
+	});
+
+	this.descriptionLayout = new OO.ui.FieldLayout( this.descriptionInput, {
+		align: 'left',
+		label: mw.message( 'bs-distributionconnector-videodropletinspector-description-label' ).plain(),
+		help: mw.message( 'bs-distributionconnector-videodropletinspector-description-help' ).plain()
+	});
+	this.dimensionLayout = new OO.ui.FieldLayout( this.dimensionsInput, {
+		align: 'left',
+		label: mw.message( 'bs-distributionconnector-videodropletinspector-dimension-label' ).plain(),
+		help: mw.message( 'bs-distributionconnector-videodropletinspector-dimension-help' ).plain()
+	});
+	this.alignmentLayout = new OO.ui.FieldLayout( this.alignmentInput, {
+		align: 'left',
+		label: mw.message( 'bs-distributionconnector-videodropletinspector-alignment-label' ).plain(),
+		help: mw.message( 'bs-distributionconnector-videodropletinspector-alignent-help' ).plain()
+	});
+	this.containerLayout = new OO.ui.FieldLayout( this.containerInput, {
+		align: 'left',
+		label: mw.message( 'bs-distributionconnector-videodropletinspector-container-label' ).plain(),
+		help: mw.message( 'bs-distributionconnector-videodropletinspector-container-help' ).plain()
 	});
 }
 
@@ -85,6 +149,21 @@ ve.ui.VideoDropletInspector.prototype.getSetupProcess = function ( data ) {
 			var attributes = this.selectedNode.getAttribute( 'mw' ).attrs;
 			this.serviceInput.getMenu().selectItemByData( attributes.service || 'youtube' );
 
+			if ( attributes.description ) {
+				this.descriptionInput.setValue( attributes.description );
+			}
+			if ( attributes.dimensions ) {
+				this.dimensionsInput.setValue( attributes.dimensions );
+			}
+
+			if ( attributes.alignment ) {
+				this.alignmentInput.getMenu().selectItemByData( attributes.alignment );
+			}
+
+			if ( attributes.container == 'frame' ) {
+				this.containerInput.setSelected( true );
+			}
+
 			this.actions.setAbilities( { done: true } );
 		}, this );
 };
@@ -93,6 +172,23 @@ ve.ui.VideoDropletInspector.prototype.updateMwData = function ( mwData ) {
 	ve.ui.VideoDropletInspector.super.prototype.updateMwData.call( this, mwData );
 
 	mwData.attrs.service = this.serviceInput.getMenu().findSelectedItem().getData();
+	mwData.attrs.alignment = this.alignmentInput.getMenu().findSelectedItem().getData();
+
+	if ( this.descriptionInput.getValue() !== '' ) {
+		mwData.attrs.description = this.descriptionInput.getValue();
+	} else {
+		delete ( mwData.attrs.description );
+	}
+	if ( this.dimensionsInput.getValue() !== '' ) {
+		mwData.attrs.dimensions = this.dimensionsInput.getValue();
+	} else {
+		delete ( mwData.attrs.dimensions );
+	}
+	if ( this.containerInput.isSelected() ) {
+		mwData.attrs.container = this.containerInput.getValue();
+	} else {
+		delete ( mwData.attrs.container );
+	}
 };
 
 /**
