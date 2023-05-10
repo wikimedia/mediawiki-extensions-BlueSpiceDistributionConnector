54b2a0dc45d89bcf4cb1a8113e087e081cf36ab0
akulbii <alk@astarios.com>
2023-05-10 10:07:47 +0300

    Migrate SimpleSAMLphp and OpenIDConnect configs to PluggableAuth 6
    [4.3]
    
    ERM32338
    
    Change-Id: If3a6e20c4c3e618074737bff9b36d1bceb7cd10b
    (cherry picked from commit f4a134e43ea659fac94670f405205a90be36c815)
    (cherry picked from commit ca6557dfffe0fe861ac5bb43cfff56b092310166)

---
+++ src/ConfigDefinition/PluggableAuth/Config.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace BlueSpice\DistributionConnector\ConfigDefinition\PluggableAuth;
+
+use BlueSpice\ConfigDefinition\ArraySetting;
+use BlueSpice\ConfigDefinition\IOverwriteGlobal;
+use BlueSpice\DistributionConnector\ISettingPaths;
+use BlueSpice\Html\FormField\KeyObjectField;
+use BlueSpice\Html\OOUI\KeyObjectInputWidget;
+use ExtensionRegistry;
+
+class Config extends ArraySetting implements ISettingPaths, IOverwriteGlobal {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getPaths() {
+		return [
+			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_AUTHENTICATION . '/PluggableAuth',
+			static::MAIN_PATH_EXTENSION . '/PluggableAuth/' . static::FEATURE_AUTHENTICATION,
+			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_FREE . '/PluggableAuth',
+		];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getLabelMessageKey() {
+		return 'bs-distributionconnector-pref-pluggableauth-config';
+	}
+
+	/**
+	 * @return KeyObjectField
+	 */
+	public function getHtmlFormField() {
+		return new KeyObjectField( $this->makeFormFieldParams() );
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	protected function makeFormFieldParams() {
+		return array_merge( parent::makeFormFieldParams(), [
+			'allowAdditions' => true,
+			'labelsOnlyOnFirst' => true,
+			'keyLabel' => $this->context->msg(
+				'bs-distributionconnector-pref-pluggableauth-config-button-label'
+			)->text(),
+			'objectConfiguration' => [
+				'plugin' => [
+					'type' => KeyObjectInputWidget::TYPE_TEXT,
+					'widget' => [
+						'required' => true,
+					],
+					'label' => $this->context->msg(
+						'bs-distributionconnector-pref-pluggableauth-config-plugin'
+					)->text()
+				],
+				'data' => [
+					'type' => KeyObjectInputWidget::TYPE_JSON,
+					'widget' => [
+						'required' => false
+					],
+					'label' => $this->context->msg(
+						'bs-distributionconnector-pref-pluggableauth-config-data-object'
+					)->text()
+				]
+			]
+		] );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function isHidden() {
+		return !ExtensionRegistry::getInstance()->isLoaded( 'PluggableAuth' );
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function getGlobalName() {
+		return 'wgPluggableAuth_Config';
+	}
+}
