60246fa838443b64304f8f816f0f08ca19273f38
driedmueller <daniel.riedmueller@hallowelt.com>
2023-10-11 14:55:29 +0200

    Reorganize menu entries
    Added Extension Replace Text link
    
    ERM34159
    [4.4.x]
    
    Depends-On: Iff4863a70c2cec9decf70ecc8b244e0f791d6c0f
    Change-Id: I4250bb11c6bcb792b5f19fbc3ff386295977fcbb
    (cherry picked from commit 976abe4a5ed0723acc3e4bb600e44ee9333340be)

---
+++ src/Hook/AddGlobalAction.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace BlueSpice\DistributionConnector\Hook;
+
+use ExtensionRegistry;
+use MediaWiki\SpecialPage\SpecialPageFactory;
+use MWStake\MediaWiki\Component\CommonUserInterface\Component\RestrictedTextLink;
+use MWStake\MediaWiki\Component\CommonUserInterface\Hook\MWStakeCommonUIRegisterSkinSlotComponents;
+use RawMessage;
+
+class AddGlobalAction implements MWStakeCommonUIRegisterSkinSlotComponents {
+
+	/** @var SpecialPageFactory */
+	private $specialPageFactory;
+
+	/**
+	 * @param SpecialPageFactory $specialPageFactory
+	 */
+	public function __construct( SpecialPageFactory $specialPageFactory ) {
+		$this->specialPageFactory = $specialPageFactory;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onMWStakeCommonUIRegisterSkinSlotComponents( $registry ): void {
+		// If ReplaceText is not available, do not register the link
+		if ( !ExtensionRegistry::getInstance()->isLoaded( 'Replace Text' ) ) {
+			return;
+		}
+
+		$special = $this->specialPageFactory->getPage( 'ReplaceText' );
+		$registry->register(
+			'GlobalActionsEditing',
+			[
+				'special-replacetext' => [
+					'factory' => static function () use ( $special ) {
+						return new RestrictedTextLink( [
+							'id' => 'special-replacetext',
+							'href' => $special->getPageTitle()->getLocalURL(),
+							'text' => new RawMessage( $special->getDescription() ),
+							'title' => new RawMessage( $special->getDescription() ),
+							'aria-label' => new RawMessage( $special->getDescription() ),
+							'permissions' => [ 'edit' ]
+						] );
+					}
+				]
+			]
+		);
+	}
+}
