0bb1126dab0aa808dc549bc647cc0ef387286389
Mschlindwein <schlindwein@hallowelt.com>
2024-01-12 14:20:35 +0100

    Add new page link for discovery skin
    ERM35507
    
    [4.4.x]
    
    Change-Id: I0e77c4391f8f1401b32f0760859a94bf98a42aac
    (cherry picked from commit 36024a27aff559dcb84e8a0252a632172b27d243)

---
+++ src/Hook/SkinTemplateNavigation/AddNewPageLink.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace BlueSpice\DistributionConnector\Hook\SkinTemplateNavigation;
+
+use BlueSpice\Discovery\Skin as DiscoverySkin;
+use ExtensionRegistry;
+use MediaWiki\Hook\SkinTemplateNavigation__UniversalHook;
+use MediaWiki\Permissions\PermissionManager;
+use Title;
+
+class AddNewPageLink implements SkinTemplateNavigation__UniversalHook {
+
+	/** @var PermissionManager */
+	private $permissionManager = null;
+
+	/**
+	 *
+	 * @param PermissionManager $permissionManager
+	 */
+	public function __construct( PermissionManager $permissionManager ) {
+		$this->permissionManager = $permissionManager;
+	}
+
+	/**
+	 * // phpcs:disable MediaWiki.NamingConventions.LowerCamelFunctionsName.FunctionName
+	 * @inheritDoc
+	 */
+	public function onSkinTemplateNavigation__Universal( $sktemplate, &$links ): void {
+		$skin = $sktemplate->getSkin();
+		if ( !is_a( $skin, DiscoverySkin::class, true ) ||
+			!ExtensionRegistry::getInstance()->isLoaded( 'StandardDialogs' ) ) {
+			return;
+		}
+		$user = $skin->getUser();
+		$title = $skin->getRelevantTitle();
+		if ( $title instanceof Title === false ) {
+			return;
+		}
+
+		$userCanCreatePages = $this->permissionManager->userHasRight( $user, 'createpage' );
+		if ( !$userCanCreatePages ) {
+			return;
+		}
+		$links['namespaces']['new-page'] = [
+			'text' => $sktemplate->msg( 'standarddialogs-create-button-new-page-text' ),
+			'title' => $sktemplate->msg( 'standarddialogs-create-button-new-page-title' ),
+			'href' => '',
+			'class' => 'new-page'
+		];
+		$sktemplate->getOutput()->addModules( 'ext.standardDialogs' );
+	}
+}
