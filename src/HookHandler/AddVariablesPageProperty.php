9893163a1062b41da0359ceff69f551c19016d54
hslater <hamish.slater@hallowelt.com>
2024-03-21 21:20:08 +1300

    Add tracker for Variables
    Add variables pageprop
    
    ERM36543
    
    [REL1_39, REL1_39-4.4.x, master]
    
    Change-Id: I8a646780dad2b2aa9faf240f66aa7c5101b16a5c

---
+++ src/HookHandler/AddVariablesPageProperty.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace BlueSpice\DistributionConnector\HookHandler;
+
+use MediaWiki\Content\Hook\ContentAlterParserOutputHook;
+use WikitextContent;
+
+class AddVariablesPageProperty implements ContentAlterParserOutputHook {
+
+	/**
+	 * @inheritDoc
+	 */
+	public function onContentAlterParserOutput( $content, $title, $parserOutput	) {
+		if ( $title->getContentModel() !== CONTENT_MODEL_WIKITEXT ) {
+			return;
+		}
+		if ( !( $content instanceof WikitextContent ) ) {
+			return;
+		}
+
+		$text = $content->getText();
+		$regex = '/\{\{#var/';
+
+		if ( preg_match( $regex, $text ) ) {
+			$parserOutput->setPageProperty( 'variables', 1 );
+		} else {
+			$parserOutput->unsetPageProperty( 'variables' );
+		}
+	}
+}
