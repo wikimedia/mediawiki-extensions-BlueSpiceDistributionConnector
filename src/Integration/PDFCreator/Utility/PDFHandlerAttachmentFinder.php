479ed4843da4a663ac1019e8afec1e98c7d8d43f
Daniel Vogel <daniel.vogel@hallowelt.com>
2025-11-24 09:52:42 +0100

    Improve pdfcreator integration for pdfhander
    [5.1.x]
    
    ERM45232
    
    Change-Id: I69aad92adbc91e8071352c351efe3984b88023d7

--- src/Integration/PDFCreator/Utility/PDFHandlerAttachmentFinder.php
+++ src/Integration/PDFCreator/Utility/PDFHandlerAttachmentFinder.php
@@ -5,32 +5,10 @@ namespace BlueSpice\DistributionConnector\Integration\PDFCreator\Utility;
 use DOMDocument;
 use DOMElement;
 use DOMXPath;
-use MediaWiki\Config\Config;
 use MediaWiki\Extension\PDFCreator\Utility\AttachmentFinder;
 use MediaWiki\Title\Title;
-use MediaWiki\Title\TitleFactory;
-use MediaWiki\Utils\UrlUtils;
-use RepoGroup;
 
 class PDFHandlerAttachmentFinder extends AttachmentFinder {
-
-	/** @var UrlUtils */
-	private $urlUtils;
-
-	/**
-	 * @param TitleFactory $titleFactory
-	 * @param Config $config
-	 * @param RepoGroup $repoGroup
-	 * @param UrlUtils $urlUtils
-	 */
-	public function __construct(
-		TitleFactory $titleFactory, Config $config, RepoGroup $repoGroup, UrlUtils $urlUtils
-	) {
-		$this->titleFactory = $titleFactory;
-		$this->config = $config;
-		$this->repoGroup = $repoGroup;
-		$this->urlUtils = $urlUtils;
-	}
 
 	/**
 	 * @param DOMDocument $dom
@@ -39,46 +17,23 @@ class PDFHandlerAttachmentFinder extends AttachmentFinder {
 	protected function find( DOMDocument $dom ): void {
 		$xpath = new DOMXPath( $dom );
 		// PDFHandlerThumbFinder marked thumbs with class "pdfhandler-thumb"
-		$images = $xpath->query(
-			'//img[contains(@class, "pdfhandler-thumb")]',
+		$attachments = $xpath->query(
+			'//a[contains(@class, "pdfhandler-media")]',
 			$dom
 		);
 
 		/** @var DOMElement */
-		foreach ( $images as $image ) {
-			$attachment = $image->parentNode;
+		foreach ( $attachments as $attachment ) {
 			if ( $attachment instanceof DOMElement === false ) {
 				continue;
 			}
 
-			if ( !$attachment->hasAttribute( 'href' ) ) {
+			if ( !$attachment->hasAttribute( 'data-prefixeddbkey' ) ) {
 				continue;
 			}
-			$href = $attachment->getAttribute( 'href' );
+			$prefixedDBKey = $attachment->getAttribute( 'data-prefixeddbkey' );
+			$title = $this->titleFactory->newFromDBkey( $prefixedDBKey );
 
-			$expanded = $this->urlUtils->expand( $href );
-			$expanded = urldecode( $expanded );
-
-			$titleText = '';
-			$parsedUrl = $this->urlUtils->parse( $expanded );
-			if ( isset( $parsedUrl['query'] ) ) {
-				$query = explode( '&', $parsedUrl['query'] );
-				foreach ( $query as $param ) {
-					if ( substr( trim( $param ), 0, strlen( 'title=' ) ) === 'title=' ) {
-						$titleText = substr( trim( $param ), strlen( 'title=' ) );
-
-						if ( str_contains( $titleText, '?' ) ) {
-							$titleText = substr( $titleText, 0, strpos( $titleText, '?' ) );
-						}
-					}
-				}
-			}
-
-			if ( $titleText === '' ) {
-				$titleText = substr( $parsedUrl['path'], strrpos( $parsedUrl['path'], '/' ) + 1 );
-			}
-
-			$title = $this->titleFactory->newFromText( $titleText );
 			if ( $title instanceof Title === false ) {
 				continue;
 			}
@@ -93,7 +48,7 @@ class PDFHandlerAttachmentFinder extends AttachmentFinder {
 			$attachment->setAttribute( 'href', $title->getLocalURL() );
 			$attachment->setAttribute( 'class', 'media pdfhandler-media' );
 
-			$filename = $file->getName();
+			$filename = $title->getPrefixedText();
 			$filename = $this->uncollideFilenames( $filename, $absPath );
 			$url = $attachment->getAttribute( 'href' );
 
@@ -110,28 +65,8 @@ class PDFHandlerAttachmentFinder extends AttachmentFinder {
 				}
 			}
 
-			// Embedding attachments did not work if link has a img tag as child.
-			// To solve this we add a new link in thumb caption.
-			$newAttachment = $attachment->ownerDocument->createElement( 'a', "($filename)" );
-			$newAttachment->setAttribute( 'class', 'media pdfhandler-media' );
-			$newAttachment->setAttribute( 'href', $attachment->getAttribute( 'href' ) );
-
-			$figure = $attachment->parentNode;
-
-			if ( $figure instanceof DOMElement && $figure->hasChildNodes() ) {
-				foreach ( $figure->childNodes as $childNode ) {
-					if ( $childNode->nodeName !== 'figcaption' ) {
-						continue;
-					}
-
-					if ( $childNode->nodeValue !== '' ) {
-						$childNode->nodeValue .= " ";
-					}
-
-					$childNode->appendChild( $newAttachment );
-				}
-
-			}
+			$attachment->setAttribute( 'href', "attachments/{$filename}" );
+			$attachment->setAttribute( 'data-fs-embed-file', 'true' );
 		}
 	}
 }
