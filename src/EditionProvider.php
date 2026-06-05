5525f740e1781b9d33906902cdde5d51a365684a
d.savuljesku <d.savuljesku@gmail.com>
2026-06-05 13:29:05 +0200

    Cache BS Edition during request

--- src/EditionProvider.php
+++ src/EditionProvider.php
@@ -5,9 +5,21 @@ namespace BlueSpice\DistributionConnector;
 class EditionProvider {
 
 	/**
+	 * @var string|null
+	 */
+	protected ?string $edition = '<not-set>';
+
+	/**
 	 * @return string|null if no edition info can be found
 	 */
 	public function getEdition(): ?string {
+		if ( $this->edition === '<not-set>' ) {
+			$this->edition = $this->readOutEdition();
+		}
+		return $this->edition;
+	}
+
+	private function readOutEdition(): ?string {
 		$edition = getenv( 'EDITION' );
 		if ( $edition ) {
 			return $edition;
