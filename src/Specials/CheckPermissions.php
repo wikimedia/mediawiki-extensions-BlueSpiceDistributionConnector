9d9c89bbdd7cc6f1124e6acb883db083f7705dad
Hamish <hamish.slater@hallowelt.com>
2026-09-17 22:19:16 +0700

    Replace deprecated OutputPage::setPageTitle with Message
    Use OutputPage::setPageTitleMsg
    
    ERM48053
    
    [5.1.x]
    
    Change-Id: Ie055f9f820eaafda4e4ac7db51a0c48ff107106e

--- src/Specials/CheckPermissions.php
+++ src/Specials/CheckPermissions.php
@@ -61,7 +61,7 @@ class CheckPermissions extends UnlistedSpecialPage {
 		$this->checkPermissions();
 		$this->setHeaders();
 		$out = $this->getOutput();
-		$out->setPageTitle( $this->msg( 'bs-distributionconnector-checkpermissions' ) );
+		$out->setPageTitleMsg( $this->msg( 'bs-distributionconnector-checkpermissions' ) );
 		$options = [];
 		foreach ( $this->permissionManager->getAllPermissions() as $var ) {
 			$options[$var] = $var;
